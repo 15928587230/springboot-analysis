034d39e9c1ed3d8b6ad83631a4f28db3aaa6fd9e
pjj <1005241532@qq.com>
2019-01-06 13:04:36 +0800

    _4观察者模式代码实现

--- springbootanalysis/src/main/java/owinfo/analysis/_4ObserverPattern/MyEventListener.java
+++ springbootanalysis/src/main/java/owinfo/analysis/_4ObserverPattern/MyEventListener.java
@@ -5,11 +5,17 @@ import java.util.EventObject;
 
 public interface MyEventListener extends EventListener{
 
+    /**
+     *  扩展观察者，统一执行update业务
+     */
     void update(EventObject eventObject);
 }
 
 class SendListener implements MyEventListener {
 
+    /**
+     *  更具观察者不同，执行不同的业务
+     */
     @Override
     public void update(EventObject eventObject) {
         System.out.println("观察者：" + eventObject.getSource());
