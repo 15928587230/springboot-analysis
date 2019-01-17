59111fae024183b9dfe432d0f224a4a332297375
pjj <105241532@qq.com>
2019-01-17 18:42:16 +0800

    Import注解源码分析

---
+++ springbootanalysis/src/main/java/owinfo/analysis/_11ImportAnnotation/ImportTest.java
@@ -0,0 +1,43 @@
+package owinfo.analysis._11ImportAnnotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * IOC初始化流程，beanFactoryPostProcessor工厂后置器执行ConfigurationClassPostProcessor扫描解析相关注解
+ * @see org.springframework.context.annotation.ConfigurationClassParser
+ * @see org.springframework.context.annotation.ConfigurationClassPostProcessor
+ * @see org.springframework.beans.factory.config.BeanDefinition
+ * @see org.springframework.beans.factory.BeanFactory
+ * @see org.springframework.beans.factory.config.BeanFactoryPostProcessor
+ * @see org.springframework.beans.factory.config.BeanPostProcessor
+ */
+@Import(User.class)
+@Component
+public class ImportTest implements CommandLineRunner {
+
+    @Resource
+    private User user;
+
+    @Autowired
+    private DefaultListableBeanFactory beanFactory;
+
+    @Override
+    public void run(String... args) {
+        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+        for (String beanName: beanDefinitionNames) {
+            System.out.println(beanName);
+        }
+
+        User bean = beanFactory.getBean("owinfo.analysis._11ImportAnnotation.User", User.class);
+        System.out.println(bean);
+
+        System.out.println(user);
+    }
+}
+
---
+++ springbootanalysis/src/main/java/owinfo/analysis/_11ImportAnnotation/User.java
@@ -0,0 +1,22 @@
+package owinfo.analysis._11ImportAnnotation;
+
+class User {
+    private String userName;
+    private double age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+}
