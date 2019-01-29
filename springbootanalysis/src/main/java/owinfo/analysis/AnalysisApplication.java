deb3b0bbf626e836097d5f888f92746afa71f966
pjj <1005241532@qq.com>
2019-01-29 23:14:22 +0800

    @Conditional注解的使用

--- springbootanalysis/src/main/java/owinfo/analysis/AnalysisApplication.java
+++ springbootanalysis/src/main/java/owinfo/analysis/AnalysisApplication.java
@@ -3,7 +3,7 @@ package owinfo.analysis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "owinfo.analysis._1Resource")
 public class AnalysisApplication {
 
     public static void main(String[] args) {
